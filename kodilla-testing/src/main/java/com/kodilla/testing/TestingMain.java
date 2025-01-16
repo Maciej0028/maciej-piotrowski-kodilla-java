b0a447c702ae1e3cb4a3bb5ee28bcd02845809f0
Maciej Piotrowski <maciejpiotrowski@o2.pl>
2025-01-16 10:21:47 +0100

    6.2 First Tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("Test OK.");
+        } else {
+            System.out.println("Error!");
+        }
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(5, 10);
+        if (sum == 15) {
+            System.out.println("Test OK.");
+        } else {
+            System.out.println("Error!");
+        }
+        int difference = calculator.subtract(20, 5);
+        if (difference == 15) {
+            System.out.println("Test OK.");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
