89b695d444fa1759dc3f67da827edbc06b47f530
Maciej Piotrowski <maciejpiotrowski@o2.pl>
2025-03-04 13:32:15 +0100

    13.1 Bean ForumUser

--- kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
@@ -1,13 +1,48 @@
 package com.kodilla.spring;
 
+import com.kodilla.spring.forum.ForumUser;
+import com.kodilla.spring.shape.Circle;
+import com.kodilla.spring.shape.Shape;
+import com.kodilla.spring.shape.Triangle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class KodillaSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testCircleLoadedIntoContainer() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Circle.class);
+        //When
+        String name = shape.getShapeName();
+        //Then
+        assertEquals("This is a circle.", name);
+    }
+
+    @Test
+    void testTriangleLoadedIntoContainer() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = context.getBean(Triangle.class);
+
+        //When
+        String name = shape.getShapeName();
+
+        //Then
+        assertEquals("This is a triangle.", name);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ForumUserTestSuite {
+    @Test
+    void testGetUsername() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser user = context.getBean(ForumUser.class);
+        //When
+        String name = user.getUserName();
+        //Then
+        assertEquals("John Smith", name);
+    }
+}
